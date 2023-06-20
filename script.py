755d03fbafe8b903498f4ea30d359a0a4db6f23e
ryanj <ryanjrychlak@gmail.com>
2023-06-20 09:32:41 -0500

    Added string manipulation to get out of "names" value

--- script.py
+++ script.py
@@ -2,36 +2,60 @@ from simple_salesforce import Salesforce, SalesforceLogin
 import requests
 import pandas as pd
 from io import StringIO
-# only want going forward in the future 2 years
+
+
+
 sf = Salesforce(username='nmondello@rjreliance.com.dev6',password='Monde100$', security_token='T8sz4CxJtqQfZaI38K8mks0e', domain = 'test')
-query = "SELECT pse__Project__r.pse__Opportunity__r.Sales_Channel__c FROM pse__Est_Vs_Actuals__c" #join to project table and join back to opportunity table, then pull forward sales channel
-response = sf.query(query)
-records = response["records"]
 
-for i in range(len(records)):
-    try:
-        print(records[i]["pse__Project__r"]["pse__Opportunity__r"]["Sales_Channel__c"])
-    except:
-        print("No Opportunity")
-
+#Query to get name, region, start date, estimated hours, sales channel
+#__r signifies a relationship object
+query = """
+SELECT Name, pse__Project__r.pse__Opportunity__r.pse__Region__r.Name, pse__Start_Date__c, pse__Estimated_Hours__c, pse__Project__r.pse__Opportunity__r.Sales_Channel__c FROM pse__Est_Vs_Actuals__c
+"""
+response = sf.query_all(query)
+records = response['records']
 temp = response['records']
 
 # Create a DataFrame to store the retrieved data
 df = pd.DataFrame(records)
 
-# Select the desired columns
-df = df[['Name', 'pse__Estimated_Hours__c', 'pse__Project__r']]
+# Select the desired columns and rename.
+df = df[['Name', 'pse__Project__r', 'pse__Start_Date__c', 'pse__Estimated_Hours__c', 'pse__Project__r']]
+df.columns = ['Name', 'Region', 'Start Date', 'FTE', 'Type']
 
+#Loop to get regions
 for i in range(len(temp)):
     try:
-        df.at[i, 'pse__Project__r'] = temp[i]["pse__Project__r"]["pse__Opportunity__r"]["Sales_Channel__c"]
+        df.loc[i, 'Region'] = temp[i]["pse__Project__r"]["pse__Opportunity__r"]['pse__Region__r']["Name"]
     except:
-        df.at[i, 'pse__Project__r'] = "No Opportunity"
+        df.loc[i, 'Region'] = 'Unlisted'
+
+#Loop and identify types
+for i in range(len(temp)):
+
+    #get person's name out of long name value
+    currentName = " ".join(str(temp[i]["Name"]).split("-")[-2].split()[:-1])
+    df.loc[i, "Name"] = currentName
+
+    try:
+        currentType = str(temp[i]["pse__Project__r"]["pse__Opportunity__r"]["Sales_Channel__c"])
+        if ('Partner Referral - ADP' in currentType):
+            df.loc[i, 'Type'] = 'ADP Services'
+        elif ('Partner Referral - UKG Service' in currentType):
+            df.loc[i, 'Type'] = 'UKG Services'
+        elif ('Partner Referral - UKG' in currentType):
+            df.loc[i, 'Type'] = 'UKG Direct Sales'
+        elif ('SaaS Direct Sales' in currentType):
+            df.loc[i, 'Type'] = 'SaaS Direct Sales'
+        else:
+            df.loc[i, 'Type'] =  'All Other Direct Sales'
+    except:
+        df.loc[i, 'Type'] = "No Opportunity"
 
 # Divide the "Estimate_Hours__c" values by 40
-df['pse__Estimated_Hours__c'] = df['pse__Estimated_Hours__c'] / 40.0
+df['FTE'] = df['FTE'] / 40.0
 
 # Export the data to Excel
-writer = pd.ExcelWriter('Salesforce2 Output.xlsx') #this will write the file to the same folder where this program is kept
+writer = pd.ExcelWriter('Salesforce3 Output.xlsx') #this will write the file to the same folder where this program is kept
 df.to_excel(writer,index=False,header=True)
 writer.close()
