286e67138483facb9ff890a37b6e2a4893457fe0
Nick <nmondello@hmc.edu>
2023-06-20 09:17:41 -0500

    Commented code

--- script.py
+++ script.py
@@ -7,6 +7,8 @@ from io import StringIO
 
 sf = Salesforce(username='nmondello@rjreliance.com.dev6',password='Monde100$', security_token='T8sz4CxJtqQfZaI38K8mks0e', domain = 'test')
 
+#Query to get name, region, start date, estimated hours, sales channel
+#__r signifies a relationship object
 query = """
 SELECT Name, pse__Project__r.pse__Opportunity__r.pse__Region__r.Name, pse__Start_Date__c, pse__Estimated_Hours__c, pse__Project__r.pse__Opportunity__r.Sales_Channel__c FROM pse__Est_Vs_Actuals__c
 """
@@ -17,7 +19,7 @@ temp = response['records']
 # Create a DataFrame to store the retrieved data
 df = pd.DataFrame(records)
 
-# Select the desired columns
+# Select the desired columns and rename.
 df = df[['Name', 'pse__Project__r', 'pse__Start_Date__c', 'pse__Estimated_Hours__c', 'pse__Project__r']]
 df.columns = ['Name', 'Region', 'Start Date', 'FTE', 'Type']
 
